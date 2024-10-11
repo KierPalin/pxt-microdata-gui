be8d467798e7e23053c9f27462c5c7192e71c868
KierPalin <kierpalin@gmail.com>
2024-10-11 14:02:42 +0100

    fixed missing functions in FunctionToGraph

--- liveDataViewer.ts
+++ liveDataViewer.ts
@@ -166,8 +166,8 @@ namespace microcode {
                 controller.B.id,
                 () => {
                     if (this.guiState != GUI_STATE.ZOOMED_IN) {
-                        this.app.popScene()
-                        this.app.pushScene(new Home(this.app))
+                        // this.app.popScene()
+                        // this.app.pushScene(new Home(this.app))
                     }
                     
                     else {
@@ -551,15 +551,18 @@ namespace microcode {
         }
     }
 
+    /** How many times should a line be duplicated when drawn? */
+    const PLOT_SMOOTHING_CONSTANT: number = 4
+
     export class FunctionToGraph {
-        private maxBufferSize: number
-        private totalMeasurements: number
-        private numberOfReadings: number
-        private isInEventMode: boolean
-        private lastLoggedEventDescription: string
-        private dataBuffer: number[]
-        private lastLoggedReading: number
-        private heightNormalisedDataBuffer: number[]
+        maxBufferSize: number
+        totalMeasurements: number
+        numberOfReadings: number
+        isInEventMode: boolean
+        lastLoggedEventDescription: string
+        dataBuffer: number[]
+        lastLoggedReading: number
+        heightNormalisedDataBuffer: number[]
 
         constructor() {
             this.maxBufferSize = 80
@@ -573,6 +576,7 @@ namespace microcode {
             this.heightNormalisedDataBuffer = []
         }
 
+        getName(): string {return "name"}
         getReading(): number {return 0}
         getNormalisedReading(): number {return Math.abs(this.getReading()) / (Math.abs(this.getMinimum()) + this.getMaximum())}
         getMinimum(): number {return 0;}
@@ -587,5 +591,82 @@ namespace microcode {
         // getPeriod(): number {return this.config.period;}
         // getMeasurements(): number {return this.config.measurements}
         // hasMeasurements(): boolean {return this.config.measurements > 0;}
+
+        
+        /**
+         * Change the size of the buffer used for this.dataBuffer & this.normalisedBuffer
+         * Will shift out old this.dataBuffer & this.normalisedBuffer values from the front.
+         * @param newBufferSize absolute new value for both this.dataBuffer & this.normalisedBuffer
+         */
+        setBufferSize(newBufferSize: number): void {
+            // Remove additional values if neccessary:
+            if (this.dataBuffer.length > newBufferSize) {
+                const difference = this.dataBuffer.length - newBufferSize
+                this.dataBuffer.splice(0, difference)
+                this.heightNormalisedDataBuffer.splice(0, difference)
+            }
+            this.maxBufferSize = newBufferSize
+        }
+
+        /**
+         * Add one value to this.dataBuffer, add that value normalised into this.normalisedBuffer too.
+         * No value is added if the reading is undefined (such as from a disconnected Jacdac sensor).
+         * If the (this.dataBuffer.length >= this.maxBufferSize) then then the oldest values are removed.
+         * @param fromY the offset by which the reading should be raised before adding to this.normalisedBuffer
+         * @returns 
+         */
+        readIntoBufferOnce(fromY: number): void {
+            const reading = this.getReading()
+
+            if (this.dataBuffer.length >= this.maxBufferSize || reading === undefined) {
+                this.dataBuffer.shift();
+                this.heightNormalisedDataBuffer.shift();
+            }
+
+            if (reading === undefined)
+                return
+
+            this.numberOfReadings += 1
+            const range: number = Math.abs(this.getMinimum()) + this.getMaximum();
+            this.dataBuffer.push(reading);
+            this.heightNormalisedDataBuffer.push(Math.round(Screen.HEIGHT - ((reading - this.getMinimum()) / range) * (BUFFERED_SCREEN_HEIGHT - fromY)) - fromY);
+        }
+
+        /**
+         * Populates this.normalisedBuffer with the Y position for each element in this.dataBuffer.
+         * Uses BUFFERED_SCREEN_HEIGHT.
+         * Invoked upon scrolling in the live-data-viewer.
+         * @param fromY The y value that each element should be offset by.
+         */
+        normaliseDataBuffer(fromY: number) {
+            const min = this.getMinimum()
+            const range: number = Math.abs(min) + this.getMaximum();
+
+            this.heightNormalisedDataBuffer = []
+            for (let i = 0; i < this.dataBuffer.length; i++) {
+                this.heightNormalisedDataBuffer.push(Math.round(Screen.HEIGHT - ((this.dataBuffer[i] - min) / range) * (BUFFERED_SCREEN_HEIGHT - fromY)) - fromY);
+            }
+        }
+
+        /**
+         * Default draw mode: may be overriden to accommodate multiple draw modes
+         * Each value in the data buffer is normalised and scaled to screen size per frame.
+         *      This is inefficient since only one value is added per frame
+         * @param fromX starting x coordinate
+         * @param color
+         */
+        draw(fromX: number, color: number): void {
+            for (let i = 0; i < this.heightNormalisedDataBuffer.length - 1; i++) {
+                for (let j = -(PLOT_SMOOTHING_CONSTANT / 2); j < PLOT_SMOOTHING_CONSTANT / 2; j++) {
+                    screen().drawLine(
+                        fromX + i,
+                        this.heightNormalisedDataBuffer[i] + j,
+                        fromX + i + 1,
+                        this.heightNormalisedDataBuffer[i + 1] + j,
+                        color
+                    );
+                }
+            }
+        }
     }
 }
