6a86c2888b78a779342f1d9cf5215ba947dc2495
KierPalin <45743174+KierPalin@users.noreply.github.com>
2024-10-29 20:46:08 +0000

    Updating default main.ts

--- main.ts
+++ main.ts
@@ -3,5 +3,25 @@ const calc = (arg0: microcode.GraphableFunction) => {
     app.popScene()
     app.pushScene(new microcode.LiveDataViewer(app, [arg0]))
 }
-const mMenu = new microcode.CalculatorMenu(app, calc);
-app.pushScene(mMenu)
+// const mMenu = new microcode.CalculatorMenu(app, calc);
+// app.pushScene(mMenu)
+
+const s = new microcode.Window({
+    app,
+    components: [
+        new microcode.GUITestComponent({
+            alignment: microcode.GUIComponentAlignment.TOP,
+            xOffset: 0,
+            yOffset: 0,
+        }),
+        new microcode.GUITestComponent({
+            alignment: microcode.GUIComponentAlignment.TOP,
+            xOffset: -(screen().width / 2),
+            yOffset: 0,
+            colour: 4
+        })
+    ]
+});
+
+
+app.pushScene(s)
