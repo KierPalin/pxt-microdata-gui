2cc19f0345cc95734f6b3ecadb3cad3680810347
KierPalin <45743174+KierPalin@users.noreply.github.com>
2024-11-05 21:18:51 +0000

    latest

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
-const app = new microcode.App();
-const calc = (arg0: microcode.GraphableFunction) => {
-    app.popScene()
-    app.pushScene(new microcode.LiveDataViewer(app, [arg0]))
-}
+// const app = new microcode.App();
+// const calc = (arg0: microcode.GraphableFunction) => {
+//     app.popScene()
+//     app.pushScene(new microcode.LiveDataViewer(app, [arg0]))
+// }
 // const w = new microcode.Window({
 //     app,
 //     components: [
@@ -43,57 +43,3 @@ const calc = (arg0: microcode.GraphableFunction) => {
 //     ]
 // });
 // app.pushScene(w)
-
-namespace microcode {
-    export class A extends CursorSceneWithPriorPage {
-        private btns: Button[]
-
-        constructor(app: App) {
-            super(app, () => {}, new GridNavigator(3, 4))
-
-            this.btns = []
-        }
-
-        /* override */ startup() {
-            super.startup()
-
-            let x = -40;
-            let y = -30;
-
-            for (let i = 0; i < 3; i++) {
-                for (let j = 0; j < 4; j++) {
-                    this.btns.push(new Button({
-                        parent: null,
-                        style: ButtonStyles.Transparent,
-                        icon: "green_tick",
-                        ariaId: "Done",
-                        x,
-                        y,
-                        onClick: () => {}
-                    }))
-                    x += screen().width / 4
-                }
-                y += screen().height / 3
-            }
-        }
-
-        draw() {
-            Screen.fillRect(
-                Screen.LEFT_EDGE,
-                Screen.TOP_EDGE,
-                Screen.WIDTH,
-                Screen.HEIGHT,
-                0xc
-            )
-
-            screen().printCenter("Sensor Selection", 2)
-
-            for (let i = 0; i < this.btns.length; i++)
-                this.btns[i].draw()
-
-            super.draw()
-        }
-    }
-}
-
-app.pushScene(new microcode.A(app))
