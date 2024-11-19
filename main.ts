e1c15bf29a189840bf992d101ecf1168cb5df96d
KierPalin <45743174+KierPalin@users.noreply.github.com>
2024-11-19 15:52:09 +0000

    buttonGroup implementation

--- main.ts
+++ main.ts
@@ -86,6 +86,130 @@ namespace microcode {
         }
     }
 
+    class ButtonCollection {
+        private btns: Button[][];
+        private height: number;
+        private widths: number[];
+
+        private isHidden: boolean;
+
+        private cursorBounds: Bounds;
+        private cursorOutlineColour: number;
+        private cursorRow: number;
+        private cursorCol: number;
+
+        constructor(opts: { btns: Button[][], isHidden: boolean, cursorColour: number }) {
+            this.btns = opts.btns;
+            this.isHidden = (opts.isHidden != null) ? opts.isHidden : false;
+
+            this.height = this.btns.length
+            this.widths = this.btns.map(row => row.length)
+
+            this.cursorBounds = this.btns[0][0].bounds
+            this.cursorOutlineColour = (opts.cursorColour != null) ? opts.cursorColour : 9 // Default is light blue
+            this.cursorRow = 0;
+            this.cursorCol = 0;
+
+            this.bindShieldButtons()
+        }
+
+        bindShieldButtons() {
+            control.onEvent(
+                ControllerButtonEvent.Pressed,
+                controller.right.id,
+                () => {
+                    if (this.cursorCol == this.widths[this.cursorRow])
+                        this.cursorCol = 0
+                    else
+                        this.cursorCol = (this.cursorCol + 1) % this.widths[this.cursorRow]
+                    this.updateCursor()
+                }
+            )
+
+            control.onEvent(
+                ControllerButtonEvent.Pressed,
+                controller.up.id,
+                () => {
+                    this.cursorRow = (((this.cursorRow - 1) % this.height) + this.height) % this.height; // Non-negative modulo
+
+                    // Row above could have less cols, adjust if neccessary:
+                    if (this.widths[this.cursorRow] <= this.cursorCol)
+                        this.cursorCol = this.widths[this.cursorRow] - 1
+                    this.updateCursor()
+                }
+            )
+
+            control.onEvent(
+                ControllerButtonEvent.Pressed,
+                controller.down.id,
+                () => {
+                    this.cursorRow = (this.cursorRow + 1) % this.height;
+
+                    // Row below could have less cols, adjust if neccessary:
+                    if (this.widths[this.cursorRow] <= this.cursorCol)
+                        this.cursorCol = this.widths[this.cursorRow] - 1
+                    this.updateCursor()
+                }
+            )
+
+            control.onEvent(
+                ControllerButtonEvent.Pressed,
+                controller.left.id,
+                () => {
+                    if (this.cursorCol == 0)
+                        this.cursorCol = this.widths[this.cursorRow] - 1
+                    else
+                        this.cursorCol -= 1
+                    this.updateCursor()
+                }
+            )
+
+            // click
+            const click = () => this.click()
+            control.onEvent(
+                ControllerButtonEvent.Pressed,
+                controller.A.id,
+                click
+            )
+            control.onEvent(
+                ControllerButtonEvent.Pressed,
+                controller.A.id + keymap.PLAYER_OFFSET,
+                click
+            )
+            control.onEvent(
+                ControllerButtonEvent.Pressed,
+                controller.B.id,
+                () => this.back()
+            )
+        }
+
+        click() {
+            this.btns[this.cursorRow][this.cursorCol].onClick(this.btns[this.cursorRow][this.cursorCol])
+        }
+
+        back() {
+
+        }
+
+
+        updateCursor() {
+            this.cursorBounds = this.btns[this.cursorRow][this.cursorCol].bounds;
+            this.drawCursor()
+        }
+
+        drawCursor() {
+            this.cursorBounds.drawRect(this.cursorOutlineColour)
+        }
+
+        draw() {
+            if (!this.isHidden) {
+                this.btns.forEach(btnRow => btnRow.forEach(btn => btn.draw()))
+
+                this.drawCursor()
+            }
+        }
+    }
+
     class C {
         public btns: Button[]
 
